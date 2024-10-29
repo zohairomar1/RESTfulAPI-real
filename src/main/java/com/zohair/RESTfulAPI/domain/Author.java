4080e3e7ef6c713a76ba1cd74e9ffdac0828a964
zohairomar1 <zohairomar@gmail.com>
2024-10-28 19:46:54 -0600

    add(domain of author and book)

--- src/main/java/com/zohair/RESTfulAPI/domain/Author.java
+++ src/main/java/com/zohair/RESTfulAPI/domain/Author.java
@@ -1,4 +1,27 @@
 package com.zohair.RESTfulAPI.domain;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="authors")
 public class Author {
+
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.SEQUENCE, generator="author_id_seq")
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+
 }
