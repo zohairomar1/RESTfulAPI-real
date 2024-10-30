07488bec813c080a0f83c91738fd740470795c07
zohairomar1 <zohairomar@gmail.com>
2024-10-30 13:05:53 -0600

    add(book read one endpoint)

--- src/main/java/com/zohair/RESTfulAPI/services/BookService.java
+++ src/main/java/com/zohair/RESTfulAPI/services/BookService.java
@@ -3,10 +3,13 @@ package com.zohair.RESTfulAPI.services;
 import com.zohair.RESTfulAPI.domain.entities.BookEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface BookService {
 
     BookEntity createBook(String isbn,BookEntity book);
 
     List<BookEntity> findAll();
+
+    Optional<BookEntity> findOne(String isbn);
 }
--- src/main/java/com/zohair/RESTfulAPI/services/impl/BookServiceImpl.java
+++ src/main/java/com/zohair/RESTfulAPI/services/impl/BookServiceImpl.java
@@ -6,6 +6,7 @@ import com.zohair.RESTfulAPI.services.BookService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -30,5 +31,10 @@ public class BookServiceImpl implements BookService {
                 .stream(bookRepository.findAll().spliterator(),
                         false)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<BookEntity> findOne(String isbn) {
+        return bookRepository.findById(isbn);
     }
 }
