00ce8d18e863c99120626c3ce323d3922b9647f9
zohairomar1 <zohairomar@gmail.com>
2024-10-30 12:46:08 -0600

    add(author read one endpoint)

--- src/main/java/com/zohair/RESTfulAPI/services/AuthorService.java
+++ src/main/java/com/zohair/RESTfulAPI/services/AuthorService.java
@@ -3,10 +3,13 @@ package com.zohair.RESTfulAPI.services;
 import com.zohair.RESTfulAPI.domain.entities.AuthorEntity;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface AuthorService {
 
     AuthorEntity createAuthor(AuthorEntity authorEntity);
 
     List<AuthorEntity> findAll();
+
+    Optional<AuthorEntity> findOne(Long id);
 }
--- src/main/java/com/zohair/RESTfulAPI/services/impl/AuthorServiceImpl.java
+++ src/main/java/com/zohair/RESTfulAPI/services/impl/AuthorServiceImpl.java
@@ -7,6 +7,7 @@ import com.zohair.RESTfulAPI.services.AuthorService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -31,5 +32,10 @@ public class AuthorServiceImpl implements AuthorService {
                 .spliterator(),
                 false)
                 .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<AuthorEntity> findOne(Long id) {
+        return authorRepository.findById(id);
     }
 }
